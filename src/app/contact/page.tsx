b300469551b6929b2f061483e57f1003b0cc96f2
Ahmed Nadeem Gondal <ag1858085@gmail.com>
2025-09-23 03:34:37 +0500

    Finalized the Music School Project

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,22 @@
-"use client"
-import React from "react";
+"use client";
+"use client";
+import React, { FormEvent, useState } from "react";
 import { BackgroundBeams } from "@/components/ui/background-beams";
 
 export default function page() {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>)=> {
+    e.preventDefault();
+    console.log('Submitted:', { email, message });
+    setEmail("");
+    setMessage("");
+  };
+
   return (
-    <div className="min-h-screen min-w-full bg-gray-900 py-12 pt-16">
-        <div className="min-h-screen flex flex-col justify-center items-center">
+    <div className="min-h-screen min-w-full bg-gray-900 py-12">
+        <div className="min-h-screen z-100 flex flex-col justify-center items-center">
             <div className="m-4 text-center">
                 <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">Contact Us</h1>
                 <p className="text-gray-400 max-w-xl mb-8">We&apos;re here to help with any questions about our courses,
@@ -14,9 +25,27 @@ export default function page() {
             </div>
 
             <div className="flex flex-col gap-2">
-                <input className="w-xl h-16 rounded-lg bg-black px-4" type="text" name="" id="" placeholder="Your email address"/>
-                <textarea className="w-xl h-36 rounded-xl bg-black p-4" name="" id="" placeholder="Your message"></textarea>
-                <button className="mt-4 bg-teal-500 h-12 rounded-xl font-bold m hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset">Send Message</button>
+              <form onSubmit={handleSubmit} className="flex z-10 flex-col gap-2">
+                <input 
+                  type="email"
+                  value={email}
+                  onChange={(e)=> setEmail(e.target.value)}
+                  className="w-xl h-16 rounded-lg bg-black px-4"
+                  placeholder="Your email address"
+                  required
+                />
+                <textarea 
+                  value={message}
+                  onChange={(e)=> setMessage(e.target.value)}
+                  className="w-xl h-36 rounded-xl bg-black p-4" 
+                  placeholder="Your message"
+                ></textarea>
+                <button 
+                type="submit"
+                className="mt-4 w-35 bg-teal-500 h-12 rounded-xl font-bold text-sm hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset">
+                  Send Message
+                </button>
+              </form>
             </div>
         </div>
 
